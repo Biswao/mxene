3fcc6adbcde2ccdada1988db0b755070b62ea5ee
Developer_Biswa <biswaranjanpradhan@reseapro.com>
2024-12-16 16:27:05 +0530

    Updated 1st 2 sections with latest content.

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   <HeroSection />
   <AdvantagesSection />
   <CoreValuesSection />
-  {/* <TechnologyMattersSection /> */}
+  <TechnologyMattersSection />
   <WhyChooseUsSection />
   <PatentsSection />
   <VisionSection />
